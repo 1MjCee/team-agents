99e7451017718fbe7605aa7a9ee3e4766c96953c
Julius Charles <cjmatheka@gmail.com>
2025-01-01 15:16:35 +0300

    Initialize crew ai

---
+++ team_crew/src/team_crew/main.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+import sys
+import warnings
+
+from team_crew.crew import TeamCrew
+
+warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
+
+def run():
+    """
+    Run the crew.
+    """
+    inputs = {
+        'topic': 'Online Investments'
+    }
+    TeamCrew().crew().kickoff(inputs=inputs)
+
+
+def train():
+    """
+    Train the crew for a given number of iterations.
+    """
+    inputs = {
+        "topic": "Online Investments"
+    }
+    try:
+        TeamCrew().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
+
+    except Exception as e:
+        raise Exception(f"An error occurred while training the crew: {e}")
+
+def replay():
+    """
+    Replay the crew execution from a specific task.
+    """
+    try:
+        TeamCrew().crew().replay(task_id=sys.argv[1])
+
+    except Exception as e:
+        raise Exception(f"An error occurred while replaying the crew: {e}")
+
+def test():
+    """
+    Test the crew execution and returns the results.
+    """
+    inputs = {
+        "topic": "Online Investments"
+    }
+    try:
+        TeamCrew().crew().test(n_iterations=int(sys.argv[1]), openai_model_name=sys.argv[2], inputs=inputs)
+
+    except Exception as e:
+        raise Exception(f"An error occurred while replaying the crew: {e}")
---
+++ team_crew/src/team_crew/tools/custom_tool.py
@@ -0,0 +1,50 @@
+from crewai.tools import BaseTool
+from typing import Type
+from pydantic import BaseModel, Field
+from telegram import Bot
+
+
+class MyCustomToolInput(BaseModel):
+    """Input schema for MyCustomTool."""
+    argument: str = Field(..., description="Description of the argument.")
+
+class MyCustomTool(BaseTool):
+    name: str = "Name of my tool"
+    description: str = (
+        "Clear description for what this tool is useful for, you agent will need this information to use it."
+    )
+    args_schema: Type[BaseModel] = MyCustomToolInput
+
+    def _run(self, argument: str) -> str:
+        # Implementation goes here
+        return "this is an example of a tool output, ignore it and move along."
+
+
+
+class TelegramMessageInput(BaseModel):
+    message: str = Field(..., description="The message to be sent to the Telegram group.")
+
+class TelegramBotTool(BaseTool):
+    name: str = "Telegram Message Sender"
+    description: str = "Sends messages to Telegram group."
+    args_schema: type[BaseModel] = TelegramMessageInput
+
+    bot: Bot | None = None
+    group_chat_id: int | None = None
+
+    model_config = {
+        "arbitrary_types_allowed": True
+    }
+
+    def __init__(self, token: str, group_chat_id: int):
+        super().__init__()
+        self.bot = Bot(token=token)
+        self.group_chat_id = group_chat_id
+
+    def _run(self, message: str) -> str:
+        try:
+            self.bot.send_message(chat_id=self.group_chat_id, text=message)
+            return "Message sent successfully"
+        except Exception as e:
+            return f"Failed to send message: {str(e)}"
+
